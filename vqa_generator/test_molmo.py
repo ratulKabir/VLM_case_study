2eb591f36741d69cfdf0d66b8f824b243c601433
ratulKabir <raihankabirratul@gmail.com>
2025-02-16 11:34:23 +0100

    Update: minor fixes on molmo test script

--- vqa_generator/test_molmo.py
+++ vqa_generator/test_molmo.py
@@ -2,14 +2,13 @@ from transformers import AutoProcessor, AutoModelForCausalLM, GenerationConfig
 import torch
 from PIL import Image
 
-# Load model and processor
-model_name = "allenai/Molmo-7B-D-0924"
 # load the processor
 processor = AutoProcessor.from_pretrained(
     'allenai/Molmo-7B-D-0924',
     trust_remote_code=True,
     torch_dtype='auto',
-    device_map='auto'
+    device_map='auto', 
+    use_fast=True,
 )
 
 # load the model
@@ -27,7 +26,9 @@ image = Image.open("/Users/ratul/Workstation/datasets/vlm/youtube/frames/video_1
 question = "Where is the pile, left right, ahead or no-pile?"
 
 # Prepare inputs
-inputs = processor(images=image, text=question, return_tensors="pt")
+inputs = processor.process(images=image, text=question, return_tensors="pt")
+# move inputs to the correct device and make a batch of size 1
+inputs = {k: v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
 
 # generate output; maximum 200 new tokens; stop generation when <|endoftext|> is generated
 output = model.generate_from_batch(
